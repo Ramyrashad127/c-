8e471b6f317220cf64a8627f32d247a61eb25264
abdelrahmanatefmorsy <138995406+abdelrahmanatefmorsy@users.noreply.github.com>
2024-07-22 21:11:08 +0300

    Update math2.cpp

--- math2.cpp
+++ math2.cpp
@@ -53,3 +53,52 @@ unsigned long long nCrModPFermat(unsigned long long n,
             * modInverse(fac[n - r], p) % p)
            % p;
 }
+///////////////////////////////////////// ncr with array to calc 1 time ///////////////////////////////////////////////////
+unsigned long long power(unsigned long long x,
+                         int y, int p)
+{
+    unsigned long long res = 1; // Initialize result
+
+    x = x % p; // Update x if it is more than or
+    // equal to p
+
+    while (y > 0)
+    {
+
+        // If y is odd, multiply x with result
+        if (y & 1)
+            res = (res * x) % p;
+
+        // y must be even now
+        y = y >> 1; // y = y/2
+        x = (x * x) % p;
+    }
+    return res;
+}
+
+// Returns n^(-1) mod p
+unsigned long long modInverse(unsigned long long n,
+                              int p)
+{
+    return power(n, p - 2, p);
+}
+
+vi<ll>modinv(1e6+1);
+vi<ll>fac(1e6+1);
+void calc(int p )
+{
+    vi<ll>res(1e6+1);
+    int n = 1e6;
+    fac[0] = 1;
+    for (int i = 1; i <= n; i++)
+        fac[i] = (fac[i - 1] * i) % p;
+    for (int i = 1; i <=n ; ++i) {
+        modinv[i]= modInverse(fac[i],p);
+    }
+}
+ll ncr_mod(int n, int r ,int p )
+{
+    if(n==r)
+        return 1;
+    return ((fac[n] * modinv[r] % p * modinv[n-r] % p) % p);
+}
