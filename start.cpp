ffff2bd6d41a9c0c3646c50c67e73e2231002f78
Ramyrashad127 <ramy41599@gmail.com>
2025-08-05 00:27:48 +0300

    new

--- start.cpp
+++ start.cpp
@@ -1,20 +1,17 @@
-// **************  start **************
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-#define ll long long
-#define vi vector
 using namespace std;
-using namespace __gnu_pbds;
-#define ordered_set tree<int , null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
-#define IOS ios_base::sync_with_stdio(0); cin.tie(0);
-void solve() {
-    
+#define ll long long 
+#define vi vector
+#define IOS ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+void solve(){
+     
 }
-int main() {
-    IOS
+int main(){
+    // IOS
     int t = 1;
-    cin >> t;
-    while (t--)
+    //cin>>t;
+    while (t--) {
         solve();
+    }
+
 }
